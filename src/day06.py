5a9f3ff1bb6e13623fb69a3e2928daffc522b1d0
Neil Vass <neil.vass+github@gmail.com>
2022-12-06 07:39:34 +0000

    day 6 complete

--- src/day06.py
+++ src/day06.py
@@ -1,16 +1,28 @@
 def fetch_data(path):
     with open(path, 'r') as f:
-        for ln in f:
-            yield ln
+        return f.readline().rstrip()
+
+def chars_to_marker(data, window_size):
+    window_start = 0
+    while True:
+        window_end = window_start + window_size
+        if len(set(data[window_start:window_end])) == window_size:
+            return window_end
+        window_start += 1
 
 #--------------------- tests -------------------------#
 
 def test_basics():
     data = fetch_data('sample_data/day06.txt')
-    assert data == 0
+    assert data == 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
+
+def test_chars_to_marker():
+    data = fetch_data('sample_data/day06.txt')
+    assert chars_to_marker(data, window_size=14) == 19
+
 
 #-----------------------------------------------------#
 
 if __name__ == "__main__":
     data = fetch_data('data/day06.txt')
-    print('Hello, World!')
+    print(chars_to_marker(data, window_size=14))

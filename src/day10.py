672ea6f51c8bc4acda9d9b43024279c0e7af9130
Neil Vass <neil.vass+github@gmail.com>
2022-12-10 09:21:12 +0000

    Day  10 part 1 complete

--- src/day10.py
+++ src/day10.py
@@ -7,6 +7,29 @@ def fetch_data(path):
             else:
                 yield 'addx', int(vals[1]), 2
 
+def execute(data):
+    X = 1
+    for op, arg, cycles in data:
+        for _ in range(cycles):
+            yield X
+        if op == 'addx':
+            X += arg
+    yield X
+           
+def get_signal_strengths(data):
+    system = execute(data)
+    sample_during = [20, 60, 100, 140, 180, 220]
+    signal_strengths = []
+    for cycle in range(220):
+        register = next(system)
+        if cycle+1 in sample_during:
+            strength = (cycle+1) * register
+            signal_strengths.append(strength)
+    return signal_strengths
+
+
+
+
 #--------------------- tests -------------------------#
 
 def test_basics():
@@ -16,10 +39,17 @@ def test_basics():
     assert next(data) == ('addx', -5, 2)
 
 def test_small_program():
-    pass
+    data = fetch_data('sample_data/day10-small.txt')
+    assert list(execute(data)) == [1, 1, 1, 4, 4, -1]
+
+def test_larger_program():
+    data = fetch_data('sample_data/day10.txt')
+    signal_strengths = get_signal_strengths(data)
+    assert signal_strengths == [420, 1140, 1800, 2940, 2880, 3960]
+    assert sum(signal_strengths) == 13140
 
 #-----------------------------------------------------#
 
 if __name__ == "__main__":
     data = fetch_data('data/day10.txt')
-    print('Hello, World!')
+    print(sum(get_signal_strengths(data)))

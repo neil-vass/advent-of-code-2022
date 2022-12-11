73e2eeccbcdbac24ab9aa42020085c95c3727122
Neil Vass <neil.vass+github@gmail.com>
2022-12-11 11:36:36 +0000

    Day 11 part 1 complete, started part 2

--- src/day11.py
+++ src/day11.py
@@ -1,14 +1,22 @@
 import re
 import operator
 
+def manage_worry_part_1(item):
+    return item // 3
+
+def manage_worry_part_2(item):
+    return item
+
+
 class Monkey:
-    def __init__(self, items, op, test_div, on_true, on_false):
+    def __init__(self, items, op, test_div, on_true, on_false, worry_manager=manage_worry_part_1):
         self.items = [int(i) for i in items]
         self.inspect = Monkey.create_operation(*op)
         self.test_div = int(test_div)
         self.on_true =  int(on_true)
         self.on_false =  int(on_false)
-        self.inspected = 0
+        self.manage_worry = worry_manager
+        self.inspections = 0
 
     def create_operation(symbol, argument):
         ops = {'+': operator.add, '-': operator.sub, '*': operator.mul}
@@ -19,8 +27,8 @@ class Monkey:
     
     def inspect_and_throw(self, item):
         item = self.inspect(item)
-        self.inspected += 1
-        item //= 3
+        self.inspections += 1
+        item = self.manage_worry(item)
         to_monkey = self.on_false if item % self.test_div else self.on_true
         return item, to_monkey
 
@@ -28,6 +36,9 @@ class Monkey:
         for item in self.items:
             yield self.inspect_and_throw(item)
         self.items = []
+
+    def catch(self, item):
+        self.items.append(item)
 
 
 def fetch_monkeys(path):
@@ -43,6 +54,16 @@ def fetch_monkeys(path):
                     on_false = next(f).split()[-1]
             ))
     return pack
+
+def play_round(pack):
+    for monkey in pack:
+        for thrown_item, target_monkey in monkey.take_turn():
+            pack[target_monkey].catch(thrown_item)
+
+def find_active_monkeys(pack, rounds):
+    for _ in range(rounds):
+        play_round(pack)
+    return sorted(m.inspections for m in pack)[-2:]
 
 #--------------------- tests -------------------------#
 
@@ -64,12 +85,46 @@ def test_inspect_and_throw():
     pack = fetch_monkeys('sample_data/day11.txt')
     assert pack[0].inspect_and_throw(79) == (500, 3)
 
+def test_take_turn():
+    pack = fetch_monkeys('sample_data/day11.txt')
+    assert list(pack[0].take_turn()) == [(500, 3), (620, 3)]
+    assert len(pack[0].items) == 0
+    assert pack[0].inspections == 2
+
 def test_play_round():
     pack = fetch_monkeys('sample_data/day11.txt')
-    #assert pack[0].take_turn() == 0
+    play_round(pack)
+    assert pack[0].items == [20, 23, 27, 26]
+    assert pack[1].items == [2080, 25, 167, 207, 401, 1046]
+    assert pack[2].items == []
+    assert pack[3].items == []
+
+def test_find_active_monkeys():
+    pack = fetch_monkeys('sample_data/day11.txt')
+    assert find_active_monkeys(pack, rounds=20) == [101, 105]
+
+def test_part_2_early_rounds():
+    pack = fetch_monkeys('sample_data/day11.txt')
+    for monkey in pack:
+        monkey.manage_worry = manage_worry_part_2
+    play_round(pack)
+    assert pack[0].inspections == 2
+    assert pack[1].inspections == 4  
+
+    find_active_monkeys(pack, rounds=19)
+    assert pack[0].inspections == 99
+    assert pack[1].inspections == 97  
+
+def test_part_2_all_rounds():
+    pack = fetch_monkeys('sample_data/day11.txt')
+    for monkey in pack:
+        monkey.manage_worry = manage_worry_part_2
+    pass
+    assert find_active_monkeys(pack, rounds=10000) == [52166, 52013]
+
 
 #-----------------------------------------------------#
 
 if __name__ == "__main__":
-    data = fetch_monkeys('data/day11.txt')
-    print('Hello, World!')
+    pack = fetch_monkeys('data/day11.txt')
+    print(operator.mul(*find_active_monkeys(pack, rounds=20)))

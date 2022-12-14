0ad4efe9e1e155a5272273794a486ed06090b2fb
Neil Vass <neil.vass+github@gmail.com>
2022-12-14 12:21:39 +0000

    Day 14 part 1 complete

--- src/day14.py
+++ src/day14.py
@@ -2,6 +2,7 @@
 class Cave:
     def __init__(self):
         self._content = dict()
+        self._start_of_void = 0
 
     def add_rocks(self, from_x, from_y, to_x, to_y):
         start_x = min(from_x, to_x)
@@ -13,6 +14,28 @@ class Cave:
         end_y = max(from_y+1, to_y+1)
         for y in range(start_y, end_y):
             self._content[(from_x, y)] = 'rock'
+
+        self._start_of_void = max(self._start_of_void, end_y)
+
+
+    def add_sand(self, x, y):
+        while y <= self._start_of_void:
+            if (x, y+1) not in self._content:
+                x, y = x, y+1
+            elif (x-1, y+1) not in self._content:
+                x, y = x-1, y+1
+            elif (x+1, y+1) not in self._content:
+                x, y = x+1, y+1
+            else:
+                self._content[(x, y)] = 'sand'
+                return x, y
+        return None
+
+    def add_sand_until_flowing(self):
+        units = 0
+        while self.add_sand(500, 0):
+            units += 1
+        return units
 
     def __call__(self, x, y):
         return self._content.get((x,y), 'air')
@@ -48,10 +71,21 @@ def test_fetch_data():
     assert cave(495, 6) == 'air'
     assert cave(496, 6) == 'rock'
 
+def test_add_sand():
+    cave = fetch_data('sample_data/day14.txt')
+    assert cave(500, 8) == 'air'
+    assert cave.add_sand(500, 0) is not None
+    assert cave(500, 8) == 'sand'
+    assert cave.add_sand(500, 0) is not None
+    assert cave(499, 8) == 'sand'
+
+def test_add_sand_until_flowing():
+    cave = fetch_data('sample_data/day14.txt')
+    assert cave.add_sand_until_flowing() == 24
 
 
 #-----------------------------------------------------#
 
 if __name__ == "__main__":
-    data = fetch_data('data/day14.txt')
-    print('Hello, World!')
+    cave = fetch_data('data/day14.txt')
+    print(cave.add_sand_until_flowing())

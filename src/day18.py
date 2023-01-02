792f390e011bf4139aa50fbf122aa3aab5763bc5
Neil Vass <neil.vass+github@gmail.com>
2023-01-02 21:55:35 +0000

    Tidied

--- src/day18.py
+++ src/day18.py
@@ -10,12 +10,9 @@ class Scan:
         
          # Find extents
         all_x, all_y, all_z = zip(*self.cubes)
-        self.max_x = max(all_x)
-        self.min_x = min(all_x)
-        self.max_y = max(all_y)
-        self.min_y = min(all_y)
-        self.max_z = max(all_z)
-        self.min_z = min(all_z)
+        self.min_extent = Pos(min(all_x)-1, min(all_y)-1, min(all_z)-1)
+        self.max_extent = Pos(max(all_x)+1, max(all_y)+1, max(all_z)+1)
+        
     
     def surface_area(self):
         surfaces = [6 - self.sides_touching(c) for c in self.cubes]
@@ -27,38 +24,29 @@ class Scan:
 
     def get_neighbours(point):
         x,y,z = point
-        return [(x-1,y,z), (x+1,y,z), (x,y-1,z), (x,y+1,z), (x,y,z-1), (x,y,z+1)]
+        return [Pos(x-1,y,z), Pos(x+1,y,z), 
+                Pos(x,y-1,z), Pos(x,y+1,z), 
+                Pos(x,y,z-1), Pos(x,y,z+1)]
 
     def can_move_to(self, explore_from):
         moves = []
         for neighbour in Scan.get_neighbours(explore_from):
-            if ((self.min_x -1 <= neighbour[0] <= self.max_x +1) and
-                (self.min_y -1 <= neighbour[1] <= self.max_y +1) and
-                (self.min_z -1 <= neighbour[2] <= self.max_z +1) and
+            if ((self.min_extent.x <= neighbour.x <= self.max_extent.x) and
+                (self.min_extent.y <= neighbour.y <= self.max_extent.y) and
+                (self.min_extent.z <= neighbour.z <= self.max_extent.z) and
                 (neighbour not in self.cubes)):
                 moves.append(neighbour)
         return moves
 
     def exterior_surface_area(self):
-        # Steam blast across the outside.
-        # Start with a cube of steam touching one of the outside edges of one cube.
-        # Mark any touched-with-steam edges as visited.
-        # Steam expands in all directions that don't have cubes in ...
-            # Max expansion is 1 extra past the extreme x/y/z vals in the scan, 
-            # no need to go further than that.
-            # Mark any touched-with-steam edges as visited.
-
-       
-
-        # Note visited steam cubes
         area = 0
-        starting_point = (self.min_x-1, self.min_y-1, self.min_z-1)
+        starting_point = self.min_extent
         explored = {starting_point}
         queue = deque([starting_point])
         while queue:
-            v = queue.popleft()
-            area += self.sides_touching(v)
-            for neighbour in self.can_move_to(explore_from=v):
+            steam_point = queue.popleft()
+            area += self.sides_touching(steam_point)
+            for neighbour in self.can_move_to(explore_from=steam_point):
                 if neighbour not in explored:
                     explored.add(neighbour)
                     queue.append(neighbour)

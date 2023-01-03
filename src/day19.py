b2bef1456108f0b4c61e377002d2ba3a4d53f090
Neil Vass <neil.vass+github@gmail.com>
2023-01-03 19:57:02 +0000

    Day 19 working in the factory...

--- src/day19.py
+++ src/day19.py
@@ -1,8 +1,25 @@
 import re
+from collections import defaultdict
 
 class Factory:
-    def __init__(self, blueprint):
+    def __init__(self, blueprint, time_remaining):
         self.blueprint = blueprint
+        self.time_remaining = time_remaining
+        self.robots = {'ore': 1}
+        self.materials = defaultdict(int)
+
+    # What could we make next?
+    def choices(self):
+        possible_materials = set(self.robots.keys())
+        for robot_type, materials_needed in self.blueprint.items():
+            if set(materials_needed.keys()).issubset(possible_materials):
+                time_needed = 1
+                for material_type, quantity_needed in materials_needed.items():   
+                    got_in_stock = self.materials[material_type]
+                    if quantity_needed > got_in_stock:
+                        time_needed += (quantity_needed - got_in_stock) // self.robots[material_type] 
+                if time_needed <= self.time_remaining:
+                    yield (robot_type, time_needed)
 
 
 
@@ -12,10 +29,10 @@ def fetch_data(path):
             m = re.match(r'Blueprint (\d+):', ln)
             id = int(m[1])
             details = dict()
-            for robot_type, costs in re.findall(r'Each (\w+) robot costs (.+?)\.', ln):
+            for robot_type, materials_needed in re.findall(r'Each (\w+) robot costs (.+?)\.', ln):
                 details[robot_type] = dict()
-                for amount, material_type in re.findall(r'(\d+) (ore|clay|obsidian)', costs):
-                    details[robot_type][material_type] = int(amount)
+                for quantity, material_type in re.findall(r'(\d+) (ore|clay|obsidian)', materials_needed):
+                    details[robot_type][material_type] = int(quantity)
             yield id, details
 
 #--------------------- tests -------------------------#
@@ -35,6 +52,14 @@ def test_fetch_data():
         'obsidian': {'ore': 3, 'clay': 8},
         'geode': {'ore': 3, 'obsidian': 12}
     })
+
+def test_factory_initialisation():
+    blueprints = fetch_data('sample_data/day19.txt')
+    id, blueprint = next(blueprints)
+    factory = Factory(blueprint, 24)
+    assert factory.robots == {'ore': 1}
+    assert factory.materials == {}
+    assert list(factory.choices()) == [('ore', 5), ('clay', 3)]
 
 
 #-----------------------------------------------------#

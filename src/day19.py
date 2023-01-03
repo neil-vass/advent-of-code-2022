9df6320637d68f8ce063001036ae40c6d8f4558a
Neil Vass <neil.vass+github@gmail.com>
2023-01-03 23:53:54 +0000

    Day 19 better but still runs too slow

--- src/day19.py
+++ src/day19.py
@@ -1,50 +1,62 @@
 import re
 from collections import defaultdict, deque
 import copy
+import cProfile
 
 class Factory:
-    def __init__(self, blueprint, time_remaining):
+    initial_state = (24, (('ore', 1, 0),))
+
+    def __init__(self, blueprint):
         self.blueprint = blueprint
-        self.time_remaining = time_remaining
-        self.robots = defaultdict(int, {'ore': 1})
-        self.materials = defaultdict(int)
-
-    def __hash__(self):
-        return hash((self.time_remaining, repr(self.robots), repr(self.materials)))
-
-    def __eq__(self, other):
-        return self.time_remaining == other.time_remaining and repr(self.robots) == repr(other.robots) and repr(self.materials) == repr(other.materials)
 
     # What could we make next?
-    def choices(self):
+    def choices(self, state):
+        # state = (24, (('ore', 1, 0)))
+        time_remaining, current_robots_and_materials = state
+        possible_materials, _, _ = zip(*current_robots_and_materials)
+
         robots_and_times = []
-        possible_materials = set(self.robots.keys())
         for robot_type, materials_needed in self.blueprint.items():
             if set(materials_needed.keys()).issubset(possible_materials):
                 time_needed = 0
                 for material_type, quantity_needed in materials_needed.items():   
-                    got_in_stock = self.materials[material_type]
-                    if quantity_needed > got_in_stock:
-                        time_for_this_material = -((quantity_needed - got_in_stock) // -self.robots[material_type]) # Ceiling division
+                    _, robot_count, material_stock = [s for s in current_robots_and_materials if s[0] == material_type][0]
+                    if quantity_needed > material_stock:
+                        time_for_this_material = -((quantity_needed - material_stock) // -robot_count) # Ceiling division
                         time_needed = max(time_needed, time_for_this_material)
                 time_needed += 1 # Time to make the robot once all materials are ready
-                if time_needed <= self.time_remaining:
+                if time_needed <= time_remaining:
                     robots_and_times.append((robot_type, time_needed))
         return robots_and_times
 
     # Put in an order for a robot. We trust that this is one of the chocies().
-    def make(self, robot_to_make, time_needed):
-        for robot_type, robot_quantity in self.robots.items():
-            self.materials[robot_type] += (robot_quantity * time_needed)
-        for material_type, quantity_needed in self.blueprint[robot_to_make].items():
-            self.materials[material_type] -= quantity_needed
-        self.time_remaining -= time_needed
-        self.robots[robot_to_make] += 1
+    def make(self, robot_to_make, time_needed, state):
+        time_remaining, current_robots_and_materials = state
+        new_robots_and_materials = []
+        is_first_robot_of_this_type = True
+        for material_type, robot_quantity, material_stock in current_robots_and_materials:
+            material_stock += (robot_quantity * time_needed)
+            if material_type in self.blueprint[robot_to_make]:
+                material_stock -= self.blueprint[robot_to_make][material_type]
+            if material_type == robot_to_make:
+                robot_quantity += 1
+                is_first_robot_of_this_type = False
+            new_robots_and_materials.append((material_type, robot_quantity, material_stock))
+        
+        if is_first_robot_of_this_type:
+            new_robots_and_materials.append((robot_to_make, 1, 0))
+        
+        time_remaining -= time_needed
+        return (time_remaining, tuple(new_robots_and_materials))
+        
 
-    def run_out_clock(self):
-        for robot_type, robot_quantity in self.robots.items():
-            self.materials[robot_type] += (robot_quantity * self.time_remaining)
-        self.time_remaining = 0
+    def run_out_clock(self, state):
+        time_remaining, current_robots_and_materials = state
+        new_robots_and_materials = []
+        for material_type, robot_quantity, material_stock in current_robots_and_materials:
+            material_stock += (robot_quantity * time_remaining)
+            new_robots_and_materials.append((material_type, robot_quantity, material_stock))
+        return (0, tuple(new_robots_and_materials))
 
 
 def fetch_data(path):
@@ -60,21 +72,26 @@ def fetch_data(path):
             yield id, details
 
 
+def geode_count(state):
+    time_remaining, current_robots_and_materials = state
+    geodes = [material_stock for material_type, robot_quantity, material_stock 
+                in current_robots_and_materials if material_type == 'geode']
+    return geodes[0] if geodes else 0
+
 # Ask the factory to crack the most geodes it can.
-def make_good_choices(initial_factory):
+def make_good_choices(factory):
     most_geodes = 0
-    explored = {initial_factory}
-    queue = deque([initial_factory])
+    explored = {Factory.initial_state}
+    queue = deque([Factory.initial_state])
     while queue:
-        factory = queue.popleft()
-        next_moves = factory.choices()
+        state = queue.popleft()
+        next_moves = factory.choices(state)
         if len(next_moves) == 0:
-            factory.run_out_clock()
-            most_geodes = max(most_geodes, factory.materials['geode'])
+            state = factory.run_out_clock(state)
+            most_geodes = max(most_geodes, geode_count(state))
 
         for robot_to_make, time_needed in next_moves:
-            option = copy.deepcopy(factory)
-            option.make(robot_to_make, time_needed)
+            option = factory.make(robot_to_make, time_needed, state)
             if option not in explored:
                 explored.add(option)
                 queue.append(option)
@@ -98,71 +115,63 @@ def test_fetch_data():
         'geode': {'ore': 3, 'obsidian': 12}
     })
 
+
 def test_factory_initialisation():
     blueprints = fetch_data('sample_data/day19.txt')
     id, blueprint = next(blueprints)
-    factory = Factory(blueprint, 24)
-    assert factory.robots == {'ore': 1}
-    assert factory.materials == {}
-    assert factory.choices() == [('ore', 5), ('clay', 3)]
+    factory = Factory(blueprint)
+    assert factory.choices(Factory.initial_state) == [('ore', 5), ('clay', 3)]
 
 def test_make_robot():
     blueprints = fetch_data('sample_data/day19.txt')
     id, blueprint = next(blueprints)
-    factory = Factory(blueprint, 24)
-    factory.make('clay', 3)
-    assert factory.robots == {'ore': 1, 'clay': 1}
-    assert factory.materials == {'ore': 1}
-    assert factory.time_remaining == 21
+    factory = Factory(blueprint)
+    state = factory.make('clay', 3, Factory.initial_state)
+    assert state == (21, (('ore', 1, 1), ('clay', 1, 0)))
 
-def test_copy_factory():
-    blueprints = fetch_data('sample_data/day19.txt')
-    id, blueprint = next(blueprints)
-    factory = Factory(blueprint, 24)
-    factory2 = copy.deepcopy(factory)
-    factory2.make('clay', 3)
-    assert factory.robots == {'ore': 1}
-    assert factory2.robots == {'ore': 1, 'clay': 1}
 
 def test_example_factory_run():
     blueprints = fetch_data('sample_data/day19.txt')
     id, blueprint = next(blueprints)
-    factory = Factory(blueprint, 24)
-    assert ('clay', 3) in factory.choices()
-    factory.make('clay', 3)
-    assert ('clay', 2) in factory.choices()
-    factory.make('clay', 2)
-    assert ('clay', 2) in factory.choices()
-    factory.make('clay', 2)
+    factory = Factory(blueprint)
+    state = Factory.initial_state
+    assert ('clay', 3) in factory.choices(state)
+    state = factory.make('clay', 3, state)
+    assert ('clay', 2) in factory.choices(state)
+    state = factory.make('clay', 2, state)
+    assert ('clay', 2) in factory.choices(state)
+    state = factory.make('clay', 2, state)
 
-    assert factory.time_remaining == 17 # Minute 7
-    assert factory.materials == {'ore': 1, 'clay': 6}
-    assert factory.robots == {'ore': 1, 'clay': 3}
-    assert ('obsidian', 4) in factory.choices()
-    factory.make('obsidian', 4)
-    assert ('clay', 1) in factory.choices()
-    factory.make('clay', 1)
-    assert ('obsidian', 3) in factory.choices()
-    factory.make('obsidian', 3)
-    assert ('geode', 3) in factory.choices()
-    factory.make('geode', 3)
+    assert state == (17, (('ore', 1, 1), ('clay', 3, 6))) # Minute 7
+    assert ('obsidian', 4) in factory.choices(state)
+    state = factory.make('obsidian', 4, state)
+    assert ('clay', 1) in factory.choices(state)
+    state = factory.make('clay', 1, state)
+    assert ('obsidian', 3) in factory.choices(state)
+    state = factory.make('obsidian', 3, state)
+    assert ('geode', 3) in factory.choices(state)
+    state = factory.make('geode', 3, state)
 
-    assert factory.time_remaining == 6 # Minute 18
-    assert ('geode', 3) in factory.choices()
-    factory.make('geode', 3)
-    factory.run_out_clock()
-    assert factory.materials['geode'] == 9
+    assert state[0] == 6 # Minute 18
+    assert ('geode', 3) in factory.choices(state)
+    state = factory.make('geode', 3, state)
+    state = factory.run_out_clock(state)
+    assert geode_count(state) == 9
 
-
-
-def __test_make_good_choices():
+# Runs for lower 'time_remaining' values (up to 18 is very fast, then gets slower and slower)
+def test_make_good_choices():
     blueprints = fetch_data('sample_data/day19.txt')
     id, blueprint = next(blueprints)
-    factory = Factory(blueprint, 24)
+    factory = Factory(blueprint)
     assert make_good_choices(factory) == 9
 
 #-----------------------------------------------------#
 
+def foo():
+    blueprints = fetch_data('sample_data/day19.txt')
+    id, blueprint = next(blueprints)
+    factory = Factory(blueprint)
+    print(f'Geodes: {make_good_choices(factory)}')
+
 if __name__ == "__main__":
-    data = fetch_data('data/day19.txt')
-    print('Hello, World!')
+    cProfile.run('foo()', sort='cumulative')

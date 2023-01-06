5191d4c02d34d8cba35de9d8f01ab9eba7a1ceb5
Neil Vass <neil.vass+github@gmail.com>
2023-01-06 17:38:33 +0000

    Day 20 part 1 working

--- src/day20.py
+++ src/day20.py
@@ -11,19 +11,16 @@ def fetch_data(path):
 def mix_step(data, idx_in_data, remix):
     current_pos = remix.index(idx_in_data)
     distance_to_move = data[idx_in_data]
-    if distance_to_move > 0:
-        distance_to_move += 1 
-    new_pos = ((current_pos + distance_to_move) % len(remix))
+    if distance_to_move == 0:
+        return
+
+    remix.pop(current_pos)
+    new_pos = (current_pos + distance_to_move) % len(remix)
     if new_pos == 0:
         new_pos = len(remix)
-
     remix.insert(new_pos, idx_in_data)
-    if new_pos > current_pos:
-        del remix[current_pos]
-    else:
-        del remix[current_pos+1]
 
-
+  
 def decrypt_data_file(data, steps=None):
     if steps is None:
         steps = len(data)
@@ -31,31 +28,10 @@ def decrypt_data_file(data, steps=None):
     remix = list(range(len(data)))
     for idx in range(steps):
         mix_step(data, idx, remix)
-    
+       
     # Apply remix to data
     return [data[pos] for pos in remix]
 
-
-def basic_decrypt_data_file(data, steps=None):
-    if steps is None:
-        steps = len(data)
-
-    remix = list(range(len(data)))
-    for idx in range(steps):
-        idx_in_data = idx
-        # Mix step
-        current_pos = remix.index(idx_in_data)
-        distance_to_move = data[idx_in_data]
-        if distance_to_move != 0:
-            remix.pop(current_pos)
-            new_pos = (current_pos + distance_to_move) % len(remix)
-            if new_pos == 0:
-                new_pos = len(remix)
-            remix.insert(new_pos, idx_in_data)
-
-    
-    # Apply remix to data
-    return [data[pos] for pos in remix]
 
 def get_coordinates(decrypted):
     start_idx = decrypted.index(0)
@@ -67,17 +43,22 @@ def get_coordinates(decrypted):
 
 #--------------------- tests -------------------------#
 
+def test_basic_decryption():
+     assert decrypt_data_file([1,0,0]) == [0,1,0]
+     assert decrypt_data_file([3,0,0]) == [0,3,0]
+     assert decrypt_data_file([-1,0,0]) == [0,-1,0]
+
 def test_fetch_data():
     data = fetch_data('sample_data/day20.txt')
     assert data == [1, 2, -3, 3, -2, 0, 4]
 
 def test_decrypt_data_step_by_step():
     data = fetch_data('sample_data/day20.txt')
-    assert basic_decrypt_data_file(data, steps=1) == [2, 1, -3, 3, -2, 0, 4]
-    assert basic_decrypt_data_file(data, steps=2) == [1, -3, 2, 3, -2, 0, 4]
-    assert basic_decrypt_data_file(data, steps=3) == [1, 2, 3, -2, -3, 0, 4]
-    assert basic_decrypt_data_file(data, steps=4) == [1, 2, -2, -3, 0, 3, 4]
-    assert basic_decrypt_data_file(data, steps=5) == [1, 2, -3, 0, 3, 4, -2]
+    assert decrypt_data_file(data, steps=1) == [2, 1, -3, 3, -2, 0, 4]
+    assert decrypt_data_file(data, steps=2) == [1, -3, 2, 3, -2, 0, 4]
+    assert decrypt_data_file(data, steps=3) == [1, 2, 3, -2, -3, 0, 4]
+    assert decrypt_data_file(data, steps=4) == [1, 2, -2, -3, 0, 3, 4]
+    assert decrypt_data_file(data, steps=5) == [1, 2, -3, 0, 3, 4, -2]
 
 def test_decrypt_data_file_completely():
     data = fetch_data('sample_data/day20.txt')
@@ -88,16 +69,14 @@ def test_get_coordinates():
     decrypted = decrypt_data_file(data)
     assert get_coordinates(decrypted) == (4, -3, 2)
 
-def test_basic_decryption():
-     assert basic_decrypt_data_file([1,0,0]) == [0,1,0]
-     assert basic_decrypt_data_file([3,0,0]) == [0,3,0]
-     assert basic_decrypt_data_file([-1,0,0]) == [0,-1,0]
+
     
 
 #-----------------------------------------------------#
 
 if __name__ == "__main__":
-    decrypted = decrypt_data_file('data/day20.txt')
+    data = fetch_data('data/day20.txt')
+    decrypted = decrypt_data_file(data)
     coords = get_coordinates(decrypted)
     print(sum(coords))
     # Gets 11621 - 'your answer is too high'

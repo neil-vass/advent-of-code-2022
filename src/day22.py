de005ad9eb8ef35ebdd18f94516890e888368ef1
Neil Vass <neil.vass+github@gmail.com>
2023-01-07 22:04:26 +0000

    Day 22 horizontal wrap works, vertical wrap next

--- src/day22.py
+++ src/day22.py
@@ -4,12 +4,40 @@ def fetch_data(path):
         lines = f.readlines()
         return [ln.rstrip() for ln in lines[:-2]], lines[-1].rstrip()
 
+def neighbours(x,y):
+    yield (x-1, y)
+    yield (x+1, y)
+    yield (x, y-1)
+    yield (x, y+1)
 
-def create_map(maplines):
+
+def create_board_map(maplines):
+    board = {}
     for x, ln in enumerate(maplines):
-            for y, char in enumerate(ln.rstrip()):
-                if char == '.':
-                    map[(x,y)] = []
+        first = last = None
+        for y, char in enumerate(ln.rstrip()):
+            if char == ' ':
+                continue
+            elif char == '.':
+                board[(x,y)] = set()
+                for neighbour in neighbours(x,y):
+                    if neighbour in board and board[neighbour] != 'wall':
+                        board[(x,y)].add(neighbour)
+                        board[neighbour].add((x,y))
+            elif char == '#':
+                board[(x,y)] = 'wall'
+
+            if first is None:
+                first = last = (x,y)
+            elif y > last[1]:
+                last = (x,y)
+            
+        # Check for wraparound
+        if board[first] != 'wall' and board[last] != 'wall':
+            board[first].add(last)
+            board[last].add(first)
+
+    return board
 
 
 
@@ -21,6 +49,35 @@ def test_basics():
     assert len(maplines) == 12
     assert maplines[0] == '        ...#'
     assert path == '10R5L5R10L4R5L5'
+
+def test_create_simple_map():
+    maplines = [
+        '..',
+        '.#'
+    ]
+    board = create_board_map(maplines)
+    assert len(board) == 4
+    assert board[0,0] == {(0,1), (1,0)}
+    assert board[0,1] == {(0,0)}
+    assert board[1,0] == {(0,0)}
+    assert board[1,1] == 'wall'
+
+def test_map_with_wraparound():
+    maplines = [
+        ' ....',
+        '  ..#',
+        ' .'
+    ]
+    board = create_board_map(maplines)
+    assert len(board) == 8
+    assert board[0,1] == {(0,2), (0,4), (2,1)}
+    assert board[0,2] == {(0,1), (0,3), (1,2)}
+    assert board[1,2] == {(0,2), (1,3)} # Wall bocks wraparound
+    assert board[2,1] == {(0,1)}
+
+    
+
+
 
 #-----------------------------------------------------#
 

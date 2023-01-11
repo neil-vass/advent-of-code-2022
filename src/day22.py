ecd0c90137ee06bbd3f90f3ff21a920433e21a75
Neil Vass <neil.vass+github@gmail.com>
2023-01-11 11:06:10 +0000

    Day 22 part 1 complete

--- src/day22.py
+++ src/day22.py
@@ -1,14 +1,9 @@
+import re
 
 def fetch_data(path):
     with open(path, 'r') as f:
         lines = f.readlines()
         return [ln.rstrip() for ln in lines[:-2]], lines[-1].rstrip()
-
-def neighbours(x,y):
-    yield (x-1, y)
-    yield (x+1, y)
-    yield (x, y-1)
-    yield (x, y+1)
 
 class Line:
     def __init__(self):
@@ -16,21 +11,21 @@ class Line:
         self.end = None
         self.walls = []
     
-    def move_endward(self, current_pos, tiles):      
+    def move_endward(self, current_pos, tiles):     
+        width = self.end - self.start + 1 
         if self.walls:
-            wrapped_walls = self.walls + [(w + self.end) for w in self.walls]
+            wrapped_walls = self.walls + [(w + width) for w in self.walls]
             farthest_move = (next(w for w in wrapped_walls if w > current_pos) -1) - current_pos
             tiles = min(tiles, farthest_move)
-        width = self.end - self.start + 1
         return ((current_pos - self.start + tiles) % width) + self.start
 
 
     def move_startward(self, current_pos, tiles):
+        width = self.end - self.start + 1
         if self.walls:
-            wrapped_walls = reversed([(w + self.end) for w in self.walls] + self.walls)
+            wrapped_walls = reversed([(w - width) for w in self.walls] + self.walls)
             farthest_move = current_pos - (next(w for w in wrapped_walls if w < current_pos) +1)
             tiles = min(tiles, farthest_move)
-        width = self.end - self.start + 1
         return ((current_pos - self.start - tiles) % width) + self.start
 
 
@@ -75,6 +70,27 @@ class Board:
         return (x,y)
 
 
+    def follow_path(self, path):
+        directions = '>v<^'
+        current_pos = (1, self.rows[1].start)
+        facing = '>'
+        history = [(current_pos, facing)]
+        for instruction in re.findall(r'(\d+|L|R)', path):
+            if instruction.isdigit():
+                current_pos = self.move(current_pos, int(instruction), facing)
+            elif instruction == 'L':
+                facing = directions[(directions.index(facing) - 1) % len(directions)]
+            elif instruction == 'R':
+                facing = directions[(directions.index(facing) + 1) % len(directions)]
+            history.append((current_pos, facing))
+        return current_pos, facing
+
+
+def final_password(path):
+    maplines, path = fetch_data(path)
+    board = Board(maplines)
+    ((row, col), facing) = board.follow_path(path)
+    return 1000 * row + 4 * col + '>v<^'.index(facing)
 
 #--------------------- tests -------------------------#
 
@@ -108,7 +124,6 @@ def test_move_within_board_limits():
     assert board.move((1,2), 1, 'v') == (1,2) # Blocked by wall
     assert board.move((2,3), 1, '^') == (1,3)
     
-
 def test_map_with_wraparound():
     board = Board([
         ' ....',
@@ -119,10 +134,24 @@ def test_map_with_wraparound():
     assert board.move((2,3), 1, 'v') == (1,3) # Pop out at top
     assert board.move((1,5), 5, 'v') == (1,5) # Blocked immediately
 
+def test_hit_walls_after_wraparound():
+    maplines, path = fetch_data('sample_data/day22.txt')
+    board = Board(maplines)
+    assert board.move((6,4), 5, 'v') == (8,4) # Wall at top blocks wraparound
+    assert board.move((6,3), 5, '^') == (8,3) # Wrap off top, then wall below where you started blocks
+    assert board.move((7,10), 20, '>') == (7,2) # Wrap and make sure we hit first wall
+    assert board.move((7,2), 20, '<') == (7,9) # As above, other way
 
+
+def test_follow_path():
+    maplines, path = fetch_data('sample_data/day22.txt')
+    board = Board(maplines)
+    assert board.follow_path(path) == ((6,8), '>')
+
+def test_final_password():
+    assert final_password('sample_data/day22.txt') == 6032
 
 #-----------------------------------------------------#
 
 if __name__ == "__main__":
-    data = fetch_data('data/day22.txt')
-    print('Hello, World!')
+    print(final_password('data/day22.txt'))

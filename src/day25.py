bd755afb30c52615ca9198691b143ada6e116f34
Neil Vass <neil.vass+github@gmail.com>
2023-01-29 00:07:06 +0000

    Day 25 complete

--- src/day25.py
+++ src/day25.py
@@ -1,28 +1,30 @@
 def fetch_data(path):
     with open(path, 'r') as f:
         for ln in f:
-            yield ln
+            yield ln.rstrip()
 
 char_to_digit = {'0':0, '1':1, '2':2, '-':-1, '=':-2}
 digit_to_char = {v:k for k,v in char_to_digit.items()}
 
-def think(target, so_far, idx):
+def build_snafu(target, so_far, idx):
     if target == 0:
         return so_far
     # what can I remove from target so it becomes a multiple of the next idx?
     for digit in (-2, -1, 0, 1, 2):
         value = digit * (5**idx)
         if ((target-value) % (5**(idx+1))) == 0:
-            return think(target-value, digit_to_char[digit] + so_far, idx+1)
-
+            return build_snafu(target-value, digit_to_char[digit] + so_far, idx+1)
 
 def to_snafu(n):
-    return think(n, '', 0)
+    return build_snafu(n, '', 0)
         
-
 def to_decimal(snafu):
     snafu = reversed(snafu)
     return sum(char_to_digit[n]*(5**idx) for idx, n in enumerate(snafu))
+
+def calculate_fuel(data):
+    total = sum(to_decimal(snafu) for snafu in data)
+    return to_snafu(total)
 
 #--------------------- tests -------------------------#
 
@@ -40,7 +42,6 @@ def test_to_snafu():
     assert to_snafu(15) == '1=0'
     assert to_snafu(314159265) == '1121-1110-1=0'
 
-
 def test_to_decimal():
     assert to_decimal('12') == 7
     assert to_decimal('112') == 32
@@ -48,9 +49,12 @@ def test_to_decimal():
     assert to_decimal('1=-1=') == 353
     assert to_decimal('1=-0-2') == 1747
 
+def test_calculate_fuel():
+    data = fetch_data('sample_data/day25.txt')
+    assert calculate_fuel(data) == '2=-1=0'
 
 #-----------------------------------------------------#
 
 if __name__ == "__main__":
     data = fetch_data('data/day25.txt')
-    print('Hello, World!')
+    print(calculate_fuel(data))
